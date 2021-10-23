eed1c104fac203634dbe1ab163ccf897d33844c9
Muginga <74222775+muginga72@users.noreply.github.com>
2021-10-23 00:13:28 -0400

    My first card App.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './App.css'
+
+const Box = () => {
+  return (
+    <div className="box">
+      <div className="red-square"></div>
+      <p className="text-bold">#FF6663</p>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Box />
+      <Box />
+      <Box />
     </div>
   );
 }
